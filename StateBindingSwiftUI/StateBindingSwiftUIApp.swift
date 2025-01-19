9ba9083b2ed4f9ccfefda5344a2c70d8037f050c
Donny <donnycode20@yahoo.com>
2025-01-19 12:19:46 -0800

    Initial Commit

---
+++ StateBindingSwiftUI/StateBindingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingSwiftUIApp.swift
+//  StateBindingSwiftUI
+//
+//  Created by don on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
